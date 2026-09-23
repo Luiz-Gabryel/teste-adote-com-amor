4b529ed67840ced344e7e02340c4b46ac9594575
Luiz-Gabryel <luizgabryel515@gmail.com>
2026-09-23 17:45:30 -0300

    subindo chave  front

---
+++ Auth/config.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = "https://riuqiczqtyndwzcwwnql.supabase.co";
+const SUPABASE_KEY = "sb_publishable_N5IH2CwdYbwNAn_dKZmkZQ_sExSWDRO";
+const SUPABASE_PASSWORD = "AdoteComAmor2026";
