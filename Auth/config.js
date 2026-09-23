bbe30afd1bc97bad9fbdc865e83e374c6c1c3087
Luiz-Gabryel <luizgabryel515@gmail.com>
2026-09-23 17:51:28 -0300

    teste

--- Auth/config.js
+++ Auth/config.js
@@ -1,3 +1,2 @@
 const SUPABASE_URL = "https://riuqiczqtyndwzcwwnql.supabase.co";
 const SUPABASE_KEY = "sb_publishable_N5IH2CwdYbwNAn_dKZmkZQ_sExSWDRO";
-const SUPABASE_PASSWORD = "AdoteComAmor2026";
